b8d5f09fa1a08638c78bad5581231bccdfe5c1f0
Jonathan Commins <joncom@gmail.com>
2015-10-18 03:30:27 -0700

    Make plugin compatible with Node

--- url-variables.js
+++ url-variables.js
@@ -11,17 +11,19 @@ ig.module('plugins.joncom.url-variables.url-variables')
     var getUrlVariables = function() {
         // An object that will hold the value of each variable, keyed by name of that variable.
         var data = {};
-        // Get the string that follows the "?" in the window's location.
-        var search = window.location.search;
-        if(search) {
-            search = search.substr(1); // Drop the leading "?"
-            var varPairs = search.split("&"); // Extract pairs in the form "foo=bar".
-            // Extract variable name and value from each pair.
-            for(var i=0; i<varPairs.length; i++) {
-                var varNameAndValue = varPairs[i].split("=");
-                var varName = varNameAndValue[0];
-                var varValue = varNameAndValue[1];
-                data[varName] = varValue; // Store variable.
+        if(window && window.location) {
+            // Get the string that follows the "?" in the window's location.
+            var search = window.location.search;
+            if(search) {
+                search = search.substr(1); // Drop the leading "?"
+                var varPairs = search.split("&"); // Extract pairs in the form "foo=bar".
+                // Extract variable name and value from each pair.
+                for(var i=0; i<varPairs.length; i++) {
+                    var varNameAndValue = varPairs[i].split("=");
+                    var varName = varNameAndValue[0];
+                    var varValue = varNameAndValue[1];
+                    data[varName] = varValue; // Store variable.
+                }
             }
         }
         return data;
